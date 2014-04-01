a2659a950a0a489c82bbaff1575382fc28e0f26a
michaljan <michal.janicki@live.co.uk>
2014-04-01 11:28:39 +0100

    Revert ""
    This reverts commit 11306c26974766938688c09f7139a5641efbdfa9.

--- src/Candidate/BoxPacker/TestPackedBox.php
+++ src/Candidate/BoxPacker/TestPackedBox.php
@@ -3,9 +3,9 @@ class TestPackedBox implements \Propcom\BoxPacker\PackedBox{
     private $box;
     private $items;
     private $weight;
-    public function __construct($box,$items,$weight) {
+    public function __construct($box,$packedItems,$weight) {
         $this->box=$box;
-        $this->items[]=$items;
+        $this->packedItems[]=$packedItems;
         $this->weight=$weight;
     }
     public function getWeight() {
